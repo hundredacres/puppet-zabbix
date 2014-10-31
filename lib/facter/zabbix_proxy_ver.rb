9b863957fcbd7c858a5b011f172693fddab18c64
Matthew Schmitt <mschmitt@sugarsync.com>
2014-10-31 14:51:44 -0700

    Fixing syntax issue with missing double quote

--- lib/facter/zabbix_proxy_ver.rb
+++ lib/facter/zabbix_proxy_ver.rb
@@ -3,7 +3,7 @@ Facter.add("zabbix_proxy_version") do
       distid = Facter.value('lsbdistid')
       case distid
       when /RedHatEnterprise|CentOS|Fedora/
-          Facter::Util::Resolution.exec('/bin/rpm -q --qf "%{VERSION} zabbix-proxy')
+          Facter::Util::Resolution.exec('/bin/rpm -q --qf "%{VERSION}" zabbix-proxy')
       when "ubuntu"
           Facter::Util::Resolution.exec('/bin/dpkg-query -W -f \'${version}\n\' zabbix-proxy-sqlite3 | cut -d":" -f2')
           "debian"
